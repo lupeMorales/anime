ff0751704e4cf355e1b102d378043b7bb34d9e1e
lupeMorales <morales.lupegmail.com>
2022-07-15 18:40:45 +0200

    add function click btnSearch

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,36 @@
 "use strict";
 const inputSearch = document.querySelector(".js-inputSearch");
 const filmsSearched = document.querySelector(".js-filmSearched");
+const btnSearch = document.querySelector(".js-btnSearch");
 let filmList = []; //array guarda resultado del fetch
 
-fetch(`https://api.jikan.moe/v4/anime?q=${inputSearch.value}`)
-  .then((response) => response.json())
-  .then((json) => {
-    console.log(json);
-    filmList = json.data;
+//funciones
+function callFetch() {
+  fetch(`https://api.jikan.moe/v4/anime?q=${inputSearch.value}`)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      filmList = json.data;
 
-    renderCard();
-  });
+      renderCard();
+    });
+}
+
 function renderCard() {
   let html = "";
   filmList.forEach((item) => {
+    html += `<article class="film-card">`;
     html += `<article class="film-card">
-        <img src="" alt="movie cover"  />
+        <img src="" alt=""  />
         <h3>${item.title}</h3>
       </article>`;
   });
   filmsSearched.innerHTML = html;
 }
+function handleClick(ev) {
+  ev.preventDefault();
+  callFetch();
+}
+//eventos
+
+btnSearch.addEventListener("click", handleClick);
